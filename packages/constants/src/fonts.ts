52bdd3927fe6711552e6dcf941a726f158e69d63
Daan Klarenbeek <github@ijskoud.dev>
2023-07-09 22:23:22 +0200

    feat: add @memory/constants

---
+++ packages/constants/src/fonts.ts
@@ -0,0 +1,4 @@
+import { Nunito_Sans, Nunito } from "next/font/google";
+
+export const NunitoSansFont = Nunito_Sans({ display: "swap", subsets: ["latin"], weight: ["300", "400", "500", "800", "900"] });
+export const NunitoFont = Nunito({ display: "swap", subsets: ["latin"], weight: ["300", "400", "500", "800", "900"] });
---
+++ packages/constants/src/types.ts
@@ -0,0 +1,4 @@
+import type React from "react";
+
+/** The div element prop types */
+export type DivElementProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
